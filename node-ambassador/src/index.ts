fad862e2a68f20aaec325042dda56db592961b4d
Angelino Valeta <angelinovaleta@gmail.com>
2022-11-25 06:20:22 +0100

    start sever

--- node-ambassador/src/index.ts
+++ node-ambassador/src/index.ts
@@ -1,1 +1,19 @@
-console.log("Hello");
+import express, { Request, Response } from "express";
+import cors from "cors";
+
+
+const app = express();
+
+
+app.use(express.json());
+app.use(cors({
+  origin: ["http://localhost:3000"]
+}))
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).send("Hello World");
+})
+
+app.listen(8000, () => {
+  console.log("listening to port 8000");
+} )
